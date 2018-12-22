4039e2d44d4eed64cc57539c9d9fc6bdc319a53c
EC2 Default User <ec2-user@ip-172-31-47-187.ec2.internal>
2018-12-22 09:13:55 +0000

    add category

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,14 +1,15 @@
 class MemosController < ApplicationController
     def index
         @memos = Memo.all
-    end
-    
-    def new
         
     end
     
+    def new
+        @categories = Category.all
+    end
+    
     def create
-        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"],category_id:params["memos"]["category_id"])
         redirect_to "/"
     end
     
@@ -26,6 +27,7 @@ class MemosController < ApplicationController
         memo = Memo.find(params["id"])
         memo.title = params["memos"]["title"]
         memo.body = params["memos"]["body"]
+         memo.category_id = params["memos"]["category_id"]
         memo.save
         redirect_to "/"
     end    
